4aca65beb4fbbec69e2cc48d348e7611482c48f9
Ravi Sharma <ravivats89@gmail.com>
2016-10-03 10:30:10 +0530

    User name in widget title

--- class-bp-extended-user-groups-widget.php
+++ class-bp-extended-user-groups-widget.php
@@ -24,8 +24,7 @@ class BP_Extended_User_Groups_Widget extends WP_Widget {
 
 	public function widget( $args, $instance ) {
 
-		$user_id   = get_current_user_id();
-
+		$user_id   = wp_get_current_user();
 		//don't show to non logged in user if is not BuddyPress user page
 
 		$defaults = array(
@@ -54,6 +53,8 @@ class BP_Extended_User_Groups_Widget extends WP_Widget {
 			if ( ! $user_id ) {
 				return '';
 			}
+
+			$instance['title'] = __( ucfirst( bp_get_displayed_user_username() ) . ' Groups', 'bp-extented-user-groups-widget' );
 		}
 
 		$title = apply_filters( 'widget_title', $instance['title'], $instance, $this->id_base );
